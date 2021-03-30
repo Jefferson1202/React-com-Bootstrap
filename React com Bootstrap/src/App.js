541aaa950e44a64431787489dac402114a58d930
Jefferson Despezzio <71894536+Jefferson1202@users.noreply.github.com>
2021-03-30 18:51:24 -0300

    Projeto

---
+++ React com Bootstrap/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Headers from './componentes/Headers';
+import Article from './componentes/Article';
+import Footer from './componentes/Footer';
+
+
+
+function App(props){
+ 
+    return(
+      <div>
+        <Headers />
+        <Article />
+        <Footer />
+      </div>
+   );
+  
+};
+
+export default App;
